49cbffe58703ddc7021a0d0b38a3e953fab307a9
Nicholas Marino <njm256@gmail.com>
2019-08-22 21:28:28 -0500

    Initial commit

---
+++ src/OpenCVTest.kt
@@ -0,0 +1,11 @@
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+
+fun main(args : Array<String>) {
+    println("Welcome to OpenCV " + Core.VERSION)
+    println(System.getProperty("java.library.path"))
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+    val m = Mat.eye(3, 3, CvType.CV_8UC1)
+    println("m = " + m.dump())
+}
