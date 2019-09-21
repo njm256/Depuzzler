8f73dfd5c6c40f504683a9c53e072d69062034e1
Nicholas Marino <njm256@gmail.com>
2019-09-20 23:21:45 -0500

    trying out k means clustering to group the lines into 2 categories, but normalizing hough space currently has problems, not sure why.

--- src/Depuzzle.kt
+++ src/Depuzzle.kt
@@ -1,38 +1,73 @@
+import org.apache.commons.math3.ml.clustering.CentroidCluster
+import org.apache.commons.math3.ml.clustering.DoublePoint
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer
 import org.opencv.core.*
-import org.opencv.highgui.HighGui.*
-import org.opencv.imgcodecs.Imgcodecs.imread
 import org.opencv.imgcodecs.Imgcodecs.imwrite
-import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.*
 import kotlin.math.*
 
-fun detectEdges() {
-    val img = imread("board.jpg")
-    var imgGrey = Mat()
-    cvtColor(img, imgGrey, COLOR_BGR2GRAY)
-    var edges = Mat()
-    blur(imgGrey, imgGrey, Size(3.0, 3.0))
-    Canny(imgGrey, edges, 255.0 / 3.0, 255.0)
-    var lines = Mat()
+fun detectEdges(img : Mat, name : String) {
+    var lines : Mat
     for (i in 185..385 step 10) {
-        HoughLines(edges, lines, 1.0, kotlin.math.PI / 180, i)
-        println("thresh: " + i + " lines: " + lines.rows())
+        lines = getHoughLines(img, i)
+        println("${name} thresh: " + i + " lines: " + lines.rows())
         var newImg = img.clone()
         for (j in 0 until lines.rows()) {
             houghLine(newImg, lines[j, 0][0], lines[j, 0][1], Scalar(0.0, 0.0, 255.0), 15)
         }
-        imwrite("board_lines_" + i + ".jpg", newImg)
+        imwrite("${name}_$i.jpg", newImg)
     }
 }
-/*
-for (i in 0 until img.cols() step 100) {
-    line(img, Point(i.toDouble(), 0.0), Point(i.toDouble(), img.rows() - 1.0), Scalar(0.0, 0.0, 255.0), 15)
-}
-imwrite("board_lines.jpg", img)
-}
-*/
 
-fun houghLine(img: Mat, r: Double, theta: Double, color: Scalar, thickness: Int): Unit {
+fun detectEdgesP(img : Mat, name : String) {
+    var lines : Mat
+    for (i in 185..385 step 10) {
+        lines = getHoughLinesP(img, i)
+        println("${name} thresh: " + i + " lines: " + lines.rows())
+        var newImg = img.clone()
+        for (j in 0 until lines.rows()) {
+            houghLineP(newImg, lines[j, 0][0], lines[j, 0][1], lines[j, 0][2], lines[j, 0][3], Scalar(0.0, 0.0, 255.0), 15)
+        }
+        imwrite("${name}_$i.jpg", newImg)
+    }
+}
+
+/*
+output is an n x 1 matrix whose entries are tuples of coordinates... i.e.,
+    [ [ x1, y1] ,
+      [ x2, y2] ,
+      ...
+      ...
+      [ xn, yn] ]
+      ...sigh.
+ */
+fun getHoughLines(img : Mat, threshold : Int) : Mat {
+    val imgGrey = Mat()
+    cvtColor(img, imgGrey, COLOR_BGR2GRAY)
+    val edges = Mat()
+    blur(imgGrey, imgGrey, Size(2.0, 2.0))
+    Canny(imgGrey, edges, 255.0 / 3.0, 255.0)
+    val lines = Mat()
+    HoughLines(edges, lines, 1.0, PI / 180, threshold)
+    return lines
+}
+
+fun getHoughLinesP(img : Mat, threshold : Int) : Mat {
+    val imgGrey = Mat()
+    cvtColor(img, imgGrey, COLOR_BGR2GRAY)
+    val edges = Mat()
+    blur(imgGrey, imgGrey, Size(3.0, 3.0))
+    Canny(imgGrey, edges, 255.0 / 3.0, 255.0)
+    val lines = Mat()
+    val len = minOf(img.rows(), img.cols()).toDouble()
+    HoughLinesP(imgGrey, lines, 3.0, 2.0 * PI / 180, threshold, len / 2.0, 5.0)
+    return lines
+}
+
+/*
+draws a line of the form (r, theta) on img. Couldn't find a simpler way to 'deHough', ugh.
+ */
+fun houghLine(img: Mat, r: Double, theta: Double, color: Scalar, thickness: Int) {
     val p1: Point
     val p2: Point
     val xint = r / cos(theta)
@@ -48,7 +83,7 @@ fun houghLine(img: Mat, r: Double, theta: Double, color: Scalar, thickness: Int)
         }
 
         if (xint < 0.0) {
-            println("r: {r}, theta: {theta}, xint: {xint}")
+            println("r: ${r}, theta: ${theta}, xint: ${xint}")
             p2 = Point(0.0, 0.0)
         } else if (xint < img.cols()) {
             p2 = Point(xint, 0.0)
@@ -61,13 +96,13 @@ fun houghLine(img: Mat, r: Double, theta: Double, color: Scalar, thickness: Int)
         } else if (yint < img.rows()) {
             p1 = Point(0.0, yint)
         } else { //yint >= img.rows()
-            println("r: {r}, theta: {theta}, yint: {yint}")
+            println("r: ${r}, theta: ${theta}, yint: ${yint}")
             p1 = Point(0.0, 0.0)
         }
 
         val oyint = yint - (img.cols() - 1.0) / tan(theta)
         if (oyint < 0) {
-            println("r: {r}, theta: {theta}, wtf.")
+            println("r: ${r}, theta: ${theta}, wtf.")
             p2 = Point(0.0, 0.0)
         } else if (oyint < img.rows()) {
             p2 = Point(img.cols() - 1.0, oyint)
@@ -78,5 +113,87 @@ fun houghLine(img: Mat, r: Double, theta: Double, color: Scalar, thickness: Int)
     line(img, p1, p2, color, thickness)
 }
 
+fun houghLineP(img : Mat, x1 : Double, y1 : Double, x2 : Double, y2 : Double, color : Scalar, thickness : Int)
+    = line(img, Point(x1, y1), Point(x2, y2), color, thickness)
 
 
+fun intersection(r1 : Double, theta1 : Double, r2 : Double, theta2 : Double)
+    = Point((r1 * sin(theta2) - r2 * sin(theta1)) / sin(theta2 - theta1)
+           ,(r1 * cos(theta2) + r2 * cos(theta1)) / sin(theta1 - theta2))
+
+fun blurGrey(img : Mat, kernel : Double, str : String) {
+    val imgGrey = Mat()
+    val edges = Mat()
+    cvtColor(img, imgGrey, COLOR_BGR2GRAY)
+    Canny(imgGrey, edges, 255.0/3.0, 255.0)
+    imwrite("${str}_${kernel}_edges.jpg", edges)
+}
+
+/*
+h is the height of the original image, which is 2 * d for d the diagonal, i.e., 2 * sqrt(img.rows()^2+img.cols()^2)
+newSize shouldn't be changed atm.
+ */
+fun normalizeHSpace(lines : Mat, h : Double, newSize : Int = 180) : Mat {
+    val newLines = lines.clone()
+    for (i in 0 until lines.rows()) {
+        newLines[i, 0][0] = lines[i, 0][0] / h * newSize
+        newLines[i, 0][1] = lines[i, 0][1] * newSize / PI
+    }
+    return newLines
+}
+
+fun denormalizeHLine(r : Double, theta: Double,  h : Double, normalized : Int = 180) : DoublePoint = DoublePoint(doubleArrayOf(r / normalized.toDouble() * h, theta * PI / normalized))
+
+fun denormalizeHSpace(lines : List<DoublePoint>, h : Double, normalized : Int = 180) : List<DoublePoint> {
+    return lines.map { DoublePoint(doubleArrayOf(it.point[0] / normalized.toDouble() * h, it.point[1] * PI / normalized)) }
+}
+
+fun pointList(lines : Mat) : MutableList<DoublePoint> {
+    val lineArray = mutableListOf<DoublePoint>()
+    for (i in 0 until lines.rows()) lineArray.add(DoublePoint(lines[i, 0]))
+    return lineArray
+}
+
+fun findClusters(lines : Mat) : MutableList<CentroidCluster<DoublePoint>>
+        = KMeansPlusPlusClusterer<DoublePoint>(2).cluster(pointList(lines))
+
+fun DoublePoint.dist(other : DoublePoint) : Double {
+    return sqrt((this.point[0] - other.point[0]).pow(2) + (this.point[1] - other.point[1]).pow(2))
+}
+
+fun sgn(x : Double) : Int = when {
+        x == 0.0 -> 0
+        x > 0.0 -> 1
+        else -> -1
+    }
+
+data class ClusterPt(val pt  : DoublePoint, val center : DoublePoint) : Comparable<ClusterPt> {
+    override operator fun compareTo(other : ClusterPt) = sgn(pt.dist(center) - other.pt.dist(other.center))
+}
+
+fun prune(pts : MutableList<CentroidCluster<DoublePoint>>, n : Int): List<DoublePoint> {
+    return pts.flatMap { cluster -> cluster.points.map {
+        ClusterPt(pt = it, center = DoublePoint(cluster.center.point))
+    } }.sorted().slice(0 until n).map { it.pt }
+}
+
+fun pruneCluster(cluster : CentroidCluster<DoublePoint>, n : Int) : List<DoublePoint>
+    = cluster.points.sortedBy { it.dist(DoublePoint(cluster.center.point)) }.slice(0 until n)
+
+fun pruneLimit(clusters : MutableList<CentroidCluster<DoublePoint>>, n : Int, lim : Int) : List<DoublePoint> {
+    val pts =  clusters.map { cluster -> cluster.points.map {
+        ClusterPt(pt = it, center = DoublePoint(cluster.center.point)) } }
+
+    var pruning = pts.map { it.size }.toMutableList()
+
+    var ptlist = pts.flatten().sortedDescending()
+    for (i in 0 until n) {
+        val index = pts.indexOfFirst { it[0].center == ptlist[0].center }
+        if (pruning[index] <= lim) {
+            break
+        }
+        pruning[index] = pruning[index] - 1
+        ptlist = ptlist.slice(1 until ptlist.size)
+    }
+    return ptlist.map { it.pt }
+}
--- src/OpenCVTest.kt
+++ src/OpenCVTest.kt
@@ -1,12 +1,48 @@
 import org.opencv.core.Core
 import org.opencv.core.CvType
 import org.opencv.core.Mat
+import org.opencv.core.Scalar
+import org.opencv.imgcodecs.Imgcodecs.imread
+import org.opencv.imgcodecs.Imgcodecs.imwrite
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main(args : Array<String>) {
     println("Welcome to OpenCV " + Core.VERSION)
     println(System.getProperty("java.library.path"))
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
-    val m = Mat.eye(3, 3, CvType.CV_8UC1)
-    println("m = " + m.dump())
-    detectEdges()
+    var img : Mat
+/*
+    for (i in 1..5) {
+        img = imread("imgs/board_${i}.jpg")
+        for (j in 1..8) {
+            blurGrey(img, j.toDouble(), "imgs/board_${i}")
+        }
+    }
+
+    img = imread("imgs/board_1.jpg")
+    detectEdgesP(img, "imgs/board_1")
+    */
+    img = imread("imgs/bwboard_empty.jpg")
+    //detectEdges(img, "imgs/bwboard")
+    val lines = getHoughLines(img, 285)
+    val h = sqrt(img.rows().toDouble().pow(2) + img.cols().toDouble().pow(2))
+    //val nlines = normalizeHSpace(lines, h)
+    println("finding clusters")
+    //val clusters = findClusters(nlines)
+    val clusters = findClusters(lines)
+    println("found ${clusters.size} clusters")
+    for (nl in clusters[0].points) {
+        //val l = denormalizeHLine(nl.point[0], nl.point[1], h)
+        val l = nl
+        println("drawing line in cluster 0: ${l.point[0]}, ${l.point[1]}")
+        houghLine(img, l.point[0], l.point[1], Scalar(0.0, 0.0, 255.0), 10)
+    }
+    for (nl in clusters[1].points) {
+        val l = nl
+        //val l = denormalizeHLine(nl.point[0], nl.point[1], h)
+        println("drawing line in cluster 1: ${l.point[0]}, ${l.point[1]}")
+        houghLine(img, l.point[0], l.point[1], Scalar(255.0, 0.0, 0.0), 10)
+    }
+    imwrite("imgs/bwboard.jpg", img)
 }
